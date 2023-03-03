07e59c7489b9a2dedbf075762211a06d44a9124a
mouravitor313 <123372633+mouravitor313@users.noreply.github.com>
2023-03-03 10:33:27 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from entrevista import Entrevista as E
+import csv
+from datetime import datetime as dt
+class main:
+    pass
+
+print(
+'''########################################################
+#           Pesquisa de mercado sobre animes           #
+########################################################'''
+)
+print('')
+pesquisa = E.Entrevistado
+E.Entrevistado(pesquisa)
+print('Obrigado!')
